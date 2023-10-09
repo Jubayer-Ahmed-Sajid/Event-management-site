456eb9620a1397d6b3e937f945e12189eab03084
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2023-10-09 18:32:19 +0600

    code modified

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -6,17 +6,40 @@ import Swal from "sweetalert2";
 
 const Login = () => {
     const navigate = useNavigate();
-    const location =useLocation();
+    const location = useLocation();
     console.log(location.state)
+    const { userLogin,createWithUserGoogle } = useContext(AuthContext)
+    const handleGoogleSignIn =()=>{
+    createWithUserGoogle()
+        .then(() => {
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Successfully logged in',
+                showConfirmButton: false,
+                timer: 2500
+            })
+            navigate(location?.state ? location.state : '/');
 
-    const {userLogin}=useContext(AuthContext)
-    const handleSignIn =e => {
-        e.preventDefault()
-        const info = new FormData(e.currentTarget);
-        const email = info.get('email')
-        const password = info.get('password')
-        // const password = e.target.password.value;
-        userLogin(email,password)
+
+        })
+        .catch((error) => {
+            Swal.fire({
+                position: 'top-end',
+                icon: 'error',
+                title: error.message,
+                showConfirmButton: false,
+                timer: 2500
+            })
+        })
+}
+const handleSignIn = e => {
+    e.preventDefault()
+    const info = new FormData(e.currentTarget);
+    const email = info.get('email')
+    const password = info.get('password')
+    // const password = e.target.password.value;
+    userLogin(email, password)
         .then(() => {
             Swal.fire({
                 position: 'top-end',
@@ -37,48 +60,51 @@ const Login = () => {
                 timer: 2500
             })
         })
-    }
-    return (
-        <div className="hero pt-24 bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
+}
+return (
+    <div className="hero pt-24 bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
 
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form onSubmit={handleSignIn} className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                            <Link to='/register' className="my-3">
-                                <p className="text-blue-700 hover:underline">New to the Site? Register</p>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <form onSubmit={handleSignIn} className="card-body">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                        <Link to='/register' className="my-3">
+                            <p className="text-blue-700 hover:underline">New to the Site? Register</p>
 
-                            </Link>
-                        </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                        </Link>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn bg-primary text-secondary ">Login</button>
 
-                        </div>
-                        <hr className="border-black font-bold my-4  w-full" />
-                        <div className="text-center flex justify-center">
-                            <h2 className="text-xl">
+                    </div>
+                    <hr className="border-black font-bold my-4  w-full" />
+                    <div className="text-center flex justify-center">
+                        <h2 className="text-xl">
+                            <button onClick={handleGoogleSignIn}>
                                 <FaGoogle></FaGoogle>
 
-                            </h2>
-                            </div>
-                    </form>
-                </div>
+                            </button>
+
+                        </h2>
+                    </div>
+                </form>
             </div>
         </div>
-    );
+    </div>
+);
 };
 
 export default Login;
--- src/Route/PrivateRoute/EventDetails.jsx
+++ src/Route/PrivateRoute/EventDetails.jsx
@@ -11,17 +11,18 @@ const EventDetails = () => {
         setDetail(eventDetails)
     },[intId,allDetails])
     console.log(detail)
-const {name,image,description,price} = detail;
+const {name,image,description,price,detailDescription} = detail;
 
     return (
         <div className="card  ">
         <figure><img src={image}/></figure>
-        <div className="card-body text-center">
+        <div className="card-body text-center p-4">
           <h2 className=" text-center text-2xl font-bold">{name}</h2>
-          <p>{description}</p>
-          <p>{price}</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Book Now</button>
+          <p className="my-2 text-xl font-semibold">{description}</p>
+          <p className="mb-2">{detailDescription}</p>
+          <p className="mb-2 font-bold">{price}</p>
+          <div className="card-actions justify-center">
+            <button className="btn bg-primary text-secondary">Book Now</button>
           </div>
         </div>
       </div>
